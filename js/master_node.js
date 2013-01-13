c2f29728aca20c6baaede84c1afd3f9343360bf1
Katsuyuki Nakagawaji <k-nkgwj@ics.es.osaka-u.ac.jp>
2013-01-13 19:17:10 +0900

    Remove redundant files

--- js/master_node.js
+++ js/master_node.js
@@ -6,6 +6,13 @@
  * To change this template use File | Settings | File Templates.
  */
 
+var mainRef = new Firebase("https://rtc.firebaseio.com/hub_test_v1/");
+//var projectName = prompt("Enter your project name.","");
+var projectName = "test";
+var projectRef = mainRef.child(projectName);
+var nextIdRef = projectRef.child("nextId");
+var nodesRef = projectRef.child("nodes");
+
 var myId,parentId;
 
 function listen(myId) {
