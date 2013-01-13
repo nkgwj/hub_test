325fec668121ce0400ae296a385811bbaa06ed2a
Katsuyuki Nakagawaji <k-nkgwj@ics.es.osaka-u.ac.jp>
2013-01-13 18:06:36 +0900

    introduces master_node.js

---
+++ js/master_node.js
@@ -0,0 +1,47 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: k-nkgwj
+ * Date: 13/01/11
+ * Time: 9:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var myId,parentId;
+var isRoot = true;
+
+$(function () {
+    nextIdRef.once('value', function (snapshot) {
+        myId = snapshot.val();
+        parentId = Math.floor(myId / 2);
+        log("Your ID:" + myId + "\nParent ID:" + parentId);
+        nextIdRef.set(myId + 1);
+
+
+        nodesRef.child(myId).child("queue").on('child_added', function (snapshot) {
+            var data = snapshot.val();
+
+            log(JSON.stringify(data));
+            snapshot.ref().remove();
+
+            switch (data.type) {
+                case "offer":
+                    incomingOffer(data.offer,data.port, data.sender);
+                    break;
+                case "answer":
+                    incomingAnswer(data.answer,data.port,data.sender);
+                    break;
+            }
+
+        });
+
+        if (!isRoot) {
+            nodesRef.child(parentId).child("queue").push({
+                type:"request",
+                sender:myId
+            });
+            initiateCall();
+
+        }
+    });
+});
