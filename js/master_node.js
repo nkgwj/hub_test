99e223ed4748a1dba21ae77ae25fd2e615197d6c
Katsuyuki Nakagawaji <k-nkgwj@ics.es.osaka-u.ac.jp>
2013-01-13 18:52:38 +0900

    introduces listen

--- js/master_node.js
+++ js/master_node.js
@@ -10,38 +10,44 @@
 var myId,parentId;
 var isRoot = true;
 
+function listen(myId) {
+    parentId = Math.floor(myId / 2);
+    log("Your ID:" + myId + "\nParent ID:" + parentId);
+    nextIdRef.set(myId + 1);
+    nodesRef.child(myId).child("queue").on('child_added', function (snapshot) {
+        var data = snapshot.val();
+        log(JSON.stringify(data));
+        snapshot.ref().remove();
+        switch (data.type) {
+            case "offer":
+                incomingOffer(data.offer, data.port, data.sender);
+                break;
+            case "answer":
+                incomingAnswer(data.answer, data.port, data.sender);
+                break;
+        }
+    });
+
+}
+
 $(function () {
-    nextIdRef.once('value', function (snapshot) {
-        myId = snapshot.val();
-        parentId = Math.floor(myId / 2);
-        log("Your ID:" + myId + "\nParent ID:" + parentId);
-        nextIdRef.set(myId + 1);
-
-
-        nodesRef.child(myId).child("queue").on('child_added', function (snapshot) {
-            var data = snapshot.val();
-
-            log(JSON.stringify(data));
-            snapshot.ref().remove();
-
-            switch (data.type) {
-                case "offer":
-                    incomingOffer(data.offer,data.port, data.sender);
-                    break;
-                case "answer":
-                    incomingAnswer(data.answer,data.port,data.sender);
-                    break;
-            }
-
+    if (isRoot) {
+        myId = 1;
+        nodesRef.remove(function(){
+            nextIdRef.set(myId, function() {
+                listen(myId);
+            });
         });
-
-        if (!isRoot) {
+    } else {
+        nextIdRef.once('value', function (snapshot) {
+            myId = snapshot.val();
+            listen(myId);
             nodesRef.child(parentId).child("queue").push({
                 type:"request",
                 sender:myId
             });
             initiateCall();
+        });
 
-        }
-    });
+    }
 });
