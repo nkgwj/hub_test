184d7953698a11715e9b62ead89eda229f9587b2
Katsuyuki Nakagawaji <k-nkgwj@ics.es.osaka-u.ac.jp>
2013-01-30 01:20:21 +0900

    Add More Heavy Task

---
+++ js/config_noverbose.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: k-nkgwj
+ * Date: 13/01/23
+ * Time: 3:32
+ * To change this template use File | Settings | File Templates.
+ */
+CONFIG = {
+  default:{
+    requestThreshold:1,
+    incrementalReduceThreshold:200,
+    requestDatasetSize:50,
+    mapSize:40,
+    reduceSize:40,
+    riseSize:40,
+    clockCycle:30
+  },
+  autoStart:true,
+  autoConnect:"auto",
+  loadMapReduceLibrary:"mapreduce/mapreduce.js",
+  verbose:false
+};
